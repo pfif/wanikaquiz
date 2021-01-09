a906dd87d83107d873bc4715e925809e6c838294
Florent Pastor <florent.pastor@gmail.com>
2021-01-09 16:34:41 +0100

    Revert "Prototype async gameloop"
    This reverts commit 66aca799f0306c9019766cab81fe7df9164a5f9b.

--- src/cljc/wanikaquiz/gameloop.cljc
+++ src/cljc/wanikaquiz/gameloop.cljc
@@ -1,23 +1,4 @@
-(ns wanikaquiz.gameloop
-  (:require
-   [clojure.core.async :as a]))
+(ns wanikaquiz.gameloop)
 
-(def gameloop-channel-out (a/chan))
-(def gameloop-channel-in (a/chan))
-
-(def current-value (atom 5))
-
-(defn start-gameloop []
-  (a/go-loop []
-    (a/<! (a/timeout 1000))
-    (swap! current-value inc)
-    (a/>! gameloop-channel-out @current-value)
-    (recur)
-    )
-  (a/go-loop []
-    (a/<! gameloop-channel-in)
-    (reset! current-value 0)
-    (a/>! gameloop-channel-out 0)
-    (recur)
-    )
-  )
+(defn return-false []
+  false)
--- src/cljs/wanikaquiz/core.cljs
+++ src/cljs/wanikaquiz/core.cljs
@@ -1,7 +1,5 @@
 (ns wanikaquiz.core
   (:require
-   [clojure.core.async :as a]
-   [wanikaquiz.gameloop :refer [gameloop-channel-out gameloop-channel-in start-gameloop]]
    [reagent.dom :as rdom]
    [re-frame.core :as re-frame]
    [wanikaquiz.events :as events]
@@ -18,32 +16,7 @@
     (rdom/unmount-component-at-node root-el)
     (rdom/render [views/main-panel] root-el)))
 
-(re-frame/reg-event-db
- :change-name
- (fn [db name]
-   (assoc db :name name)))
-
-(re-frame/reg-event-fx
- :reset-counter
- (fn [_ _]
-   {:ask-reset-counter nil}))
-
-(re-frame/reg-fx
- :ask-reset-counter
- (fn [_]
-   (a/go
-     (a/>! gameloop-channel-in 0))))
-
-(defn start-reading-from-gameloop []
-  (a/go
-    (a/go-loop []
-      (re-frame/dispatch [:change-name (a/<! gameloop-channel-out)])
-      (recur))
-    ))
-
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
-  (start-gameloop)
-  (start-reading-from-gameloop)
   (mount-root))
